5d736afb99036c7d367f960390367bbcc63a6a6b
Deng Han <han.deng@ansys.com>
2015-12-23 16:41:57 +0800

    adjust front-end-web 7

--- Gemfile
+++ Gemfile
@@ -37,7 +37,6 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 # Use ActiveModel has_secure_password
  gem 'bcrypt', '~> 3.1.7'
-
 #search tables
 gem 'jquery-datatables-rails', '~> 3.3.0'
 gem 'jquery-ui-rails', '5.0.5'
--- app/models/package_order.rb
+++ app/models/package_order.rb
@@ -1,6 +1,5 @@
 class PackageOrder < ActiveRecord::Base
 	belongs_to :user
-	default_scope -> { order('created_at DESC') }
 
 	validates :group_number, presence: true, allow_blank: true
       validates :outdate, presence: true, allow_blank: true
