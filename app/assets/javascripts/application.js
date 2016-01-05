69f0539364fff8fb3a23dd3d36648549857d2bb5
Deng Han <han.deng@ansys.com>
2016-01-05 17:25:06 +0800

    new adjust front end add on 5th Jan.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,9 +13,7 @@
 
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap
 //= require turbolinks
-//= require jquery.dataTables.min
 //= require dataTables/jquery.dataTables
 //= require jquery-1.11.1.min
 //= require jquery-ui
@@ -30,26 +28,5 @@
 //= require planetickets
 //= require finances
 
-
-
-//= require excanvas.min
-//= require custom 
-//= require jquery.ui.custom  
-//= require bootstrap.min  
-//= require jquery.flot.min  
-//= require jquery.flot.resize.min  
-//= require jquery.peity.min  
-//= require jquery.gritter.min  
-//= require matrix  
-//= require fullcalendar.min
-//= require matrix.chat  
-//= require jquery.validate  
-//= require matrix.form_validation  
-//= require jquery.wizard  
-//= require jquery.uniform  
-//= require select2.min  
-//= require matrix.popover   
-//= require matrix.tables  
-//= require matrix.interface  
 
 
